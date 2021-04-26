a558fbfe6aab552fdabf7cd31e34d60f0e84964a
Karel J. van der Lelij <kadmosb1@users.noreply.github.com>
2021-04-26 10:41:19 +0200

    Mijn eerste simpele feature heb ik ontwikkeld.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main (String [] args) {
         System.out.println ("Eerste regel code");
+        System.out.println ("Ik ben bezig met de simpele feature en de ontwikkeling daarvan");
+        System.out.println ("Hé. Zo is de software klaar.");
     }
 }
